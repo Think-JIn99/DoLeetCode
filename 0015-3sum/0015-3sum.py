cf12ebc0a3ffd307c62c9b45ca4712d0e24e2322
정명진 <thinkjin99@gmail.com>
2022-10-21 14:13:11 +0900

    Time: 756 ms (93.29%), Space: 18 MB (72.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,46 +1,32 @@
-class Solution:
-    def binary_search(self, num, nums, left):
-        # left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] < num:
-                left = mid + 1
-            elif nums[mid] > num:
-                right = mid - 1
-            elif nums[mid] == num: return mid
-        
-        return -1
-                
-        
+class Solution:                
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        min_ = nums[-1]
-        two_sum_min = nums[-2] + nums[-1]
         res = []
-        f_visited = set()
-        for i, first_num in enumerate(nums):
-            
-            if first_num > two_sum_min:
-                break
-            
-            if first_num in f_visited: continue
-            s_visited = set()
-            
-            for j, second_num in enumerate(nums[i+1:], start=i+1):
-                if second_num in s_visited: continue
+        nums.sort()
+        for i, first_num in enumerate(nums[:-2]):
+            left = i + 1
+            right = len(nums) - 1
+            if i > 0 and nums[i] == nums[i-1]: continue
+            while left < right:
+                two_sum = nums[left] + nums[right]
+                if two_sum > -first_num:
+                    right -= 1
+                  
+                elif two_sum < -first_num:
+                    left += 1
+                   
+                elif two_sum == -first_num:
+                    res.append([first_num, nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]: left += 1
+                    while left < right and nums[right] == nums[right - 1]: right -= 1
+                    left += 1
+                    right -=1
+        
+        return res
                 
-                two_sum = first_num + second_num
-                if two_sum > min_: break
-                
-                idx = self.binary_search(-two_sum, nums, j + 1)
-                
-                if idx > j:
-                    res.append([first_num, second_num, nums[idx]])
-                
-                s_visited.add(second_num)
             
-            f_visited.add(first_num)
+           
+        
+        
             
         return res
         
