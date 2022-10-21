8e65447865165d330ebd4965e8d612d4873f1ac1
정명진 <thinkjin99@gmail.com>
2022-10-21 14:18:56 +0900

    Time: 2217 ms (43.55%), Space: 18.2 MB (40.02%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,16 +5,18 @@ class Solution:
         for i, first_num in enumerate(nums[:-2]):
             left = i + 1
             right = len(nums) - 1
+            
             if i > 0 and nums[i] == nums[i-1]: continue
+            
             while left < right:
-                two_sum = nums[left] + nums[right]
-                if two_sum > -first_num:
+                sum_ = first_num + nums[left] + nums[right]
+                if sum_ > 0:
                     right -= 1
                   
-                elif two_sum < -first_num:
+                elif sum_ < 0:
                     left += 1
                    
-                elif two_sum == -first_num:
+                elif sum_ == 0:
                     res.append([first_num, nums[left], nums[right]])
                     while left < right and nums[left] == nums[left + 1]: left += 1
                     while left < right and nums[right] == nums[right - 1]: right -= 1
